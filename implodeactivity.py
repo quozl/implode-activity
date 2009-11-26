12ed2837565b216f3742bfc8609d794504549251
Joe Lee <joe@jotaro.com>
2009-11-25 21:28:09 -0600

    Updating copyright notices.

--- implodeactivity.py
+++ implodeactivity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2007, Joseph C. Lee
+# Copyright (C) 2007-2009, Joseph C. Lee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
